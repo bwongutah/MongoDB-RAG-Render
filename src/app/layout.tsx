f6c281e939f8818c00bda9e99aed771f8152375b
Bob Wong <bob.wong@nurs.utah.edu>
2025-02-19 21:50:55 -0700

    Changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Chatter",
+  title: "Diabetes Chatbot",
   description: "RAG using MongoDB Atlas & OpenAI"
 };
 
